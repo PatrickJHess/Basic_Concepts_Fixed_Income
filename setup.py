9601343ba5d32f11ab46b695d4dc97e28e6d5cdc
Patrick Hess <phess@ucsg.com>
2026-05-18 09:56:10 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,18 @@ setup(
     name='basic_concepts_fixed_income',  # The name pip will use to register it
     version='0.1.0',
     py_modules=['module_ basic_concepts_fixed_income'],       # <--- Replace with your actual module's name (e.g., 'pricing_core')
-    install_requires=required_packages,
+    install_requires=[
+        'numpy',
+        'pandas<3.0',
+        'openpyxl',
+        'pathvalidate',
+        'matplotlib',
+        'python-dateutil',
+        'holidays',
+        'ipynbname',
+        'beautifulsoup4',
+        'pandas_market_calendars',
+        'scipy',
+        'pandas-datareader'
+],
 )
