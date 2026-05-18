5023bc62461d29c008f64c975441966cf5b3bff9
Patrick Hess <phess@ucsg.com>
2026-05-18 09:41:57 -0500

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+# Read the requirements.txt file so pip knows what to install
+with open('requirements.txt') as f:
+    required_packages = f.read().splitlines()
+
+setup(
+    name='basic_concepts_fixed_income',  # The name pip will use to register it
+    version='0.1.0',
+    py_modules=['module_ basic_concepts_fixed_income'],       # <--- Replace with your actual module's name (e.g., 'pricing_core')
+    install_requires=required_packages,
+)
